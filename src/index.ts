c55a550853f042e1225fdb0d1c722f970140a871
MariamElZaatari <mariamalzaatari@gmail.com>
2022-09-07 23:37:55 +0300

    Clean up index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,14 +14,17 @@ export const run = async () => {
     utils.validateTrigger(context.eventName);
     utils.validateProjectLists(allowedProjects, blockedProjects);
 
-    
     // Check If It's a Pull Request Comment
     if (context.eventName === "issue_comment") {
       /*Construct The Comment as: 
         User commented:
         hello world!
         Comment URL -> git.com */
-      const dynamicCommentText = context.payload.comment?.user.login === "github-actions" ? `${context.payload.comment?.user.login} commented -> ${context.payload.comment?.html_url}`:`${context.payload.comment?.user.login} commented:\n\n${context.payload.comment?.body}\n\nComment URL -> ${context.payload.comment?.html_url}`;
+      console.log();
+      const dynamicCommentText =
+        context.payload.comment?.user.login === "github-actions"
+          ? `${context.payload.comment?.user.login} commented -> ${context.payload.comment?.html_url}`
+          : `${context.payload.comment?.user.login} commented:\n\n${context.payload.comment?.body}\n\nComment URL -> ${context.payload.comment?.html_url}`;
 
       const result = await axios.post(REQUESTS.ACTION_URL, {
         allowedProjects,
