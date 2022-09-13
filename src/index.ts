f71feab6bac93ead64cd195f1a4ed7eb590b12d6
MariamElZaatari <mariamalzaatari@gmail.com>
2022-09-13 04:37:26 +0300

    Clean up index

--- src/index.ts
+++ src/index.ts
@@ -28,10 +28,11 @@ export const run = async () => {
 
       if (commentBody.includes(">")) {
         const lines = commentBody.split("\n");
-        let commentBodyLines = lines
-          .filter(function (line: string | string[]) {
-            return line.indexOf(">") !== 0;
-          });
+        const commentBodyLines = lines.filter(function (
+          line: string | string[]
+        ) {
+          return line.indexOf(">") !== 0;
+        });
         commentBodyLines.shift();
         commentBody = commentBodyLines.join("");
 
