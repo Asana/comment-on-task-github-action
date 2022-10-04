66bc7125926b750b5102d30d37950e7c8cc20e62
MariamElZaatari <mariamalzaatari@gmail.com>
2022-10-05 02:04:39 +0300

    Users.ts Added

---
+++ src/constants/users.ts
@@ -0,0 +1,42 @@
+export const users = [
+  {
+    asanaId: 1992899177766, // https://app.asana.com/0/user_gid
+    asanaName: "Nathan",
+    githubName: "tylerdigital",
+  },
+  {
+    asanaId: 11332651049782,
+    asanaName: "Natalie MacLees",
+    githubName: "NatalieMac",
+  },
+  {
+    asanaId: 1172261355686366,
+    asanaName: "Natalie Garza",
+    githubName: "gnarza",
+  },
+  {
+    asanaId: 1200161861631865,
+    asanaName: "Cynthia Hug",
+    githubName: "cynhu92",
+  },
+  {
+    asanaId: 1202258098000877,
+    asanaName: "Mariam El Zaatari",
+    githubName: "MariamElZaatari",
+  },
+  {
+    asanaId: 1202395095687177,
+    asanaName: "Amin Abdulkhalek",
+    githubName: "aminabdulkhalek",
+  },
+  {
+    asanaId: 1202852821241548,
+    asanaName: "Hamze Ammar",
+    githubName: "Hamze-Ammar",
+  },
+  {
+    asanaId: 1202852825308828,
+    asanaName: "Hsein Bitar",
+    githubName: "hsein-bitar",
+  },
+];
