a9e5f9d8dcb14871b2a68dd60e024617fe4cd298
MariamElZaatari <mariamalzaatari@gmail.com>
2022-10-05 19:15:37 +0300

    Add Urls in Requests

--- src/constants/requests.ts
+++ src/constants/requests.ts
@@ -2,3 +2,5 @@ export const BASE_URL = "https://github.integrations.asana.plus/custom/v1";
 export const ACTION_URL = "actions/comment";
 export const RETRIES = 3;
 export const RETRY_DELAY = 1000;
+export const ASANA_URL = "https://app.asana.com/api/1.0/tasks/";
+export const COLLAB_URL = "/addFollowers";
