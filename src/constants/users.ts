9f0e26dc576cfe65ff38aff4716301a29c986713
MariamElZaatari <mariamalzaatari@gmail.com>
2022-10-14 00:01:08 +0300

    Change Users Asana IDs

--- src/constants/users.ts
+++ src/constants/users.ts
@@ -1,46 +1,46 @@
 export const users = [
   {
-    asanaId: "1992899177766",
+    asanaId: "1992810427453",
     asanaName: "Nathan",
     githubName: "tylerdigital",
   },
   {
-    asanaId: "11332651049782",
+    asanaId: "11332651049773",
     asanaName: "Natalie MacLees",
     githubName: "NatalieMac",
   },
   {
-    asanaId: "1172261355686366",
+    asanaId: "1172261355139211",
     asanaName: "Natalie Garza",
     githubName: "gnarza",
   },
   {
-    asanaId: "1200161861631865",
+    asanaId: "1200161861602258",
     asanaName: "Cynthia Hug",
     githubName: "cynhu92",
   },
   {
-    asanaId: "1202258098000877",
+    asanaId: "1202256129588512",
     asanaName: "Mariam El Zaatari",
     githubName: "MariamElZaatari",
   },
   {
-    asanaId: "1202395095687177",
+    asanaId: "1202393076412167",
     asanaName: "Amin Abdulkhalek",
     githubName: "aminabdulkhalek",
   },
   {
-    asanaId: "1202852821241548",
+    asanaId: "1202852151625813",
     asanaName: "Hamze Ammar",
     githubName: "Hamze-Ammar",
   },
   {
-    asanaId: "1202852825308828",
+    asanaId: "1202852209355924",
     asanaName: "Hsein Bitar",
     githubName: "hsein-bitar",
   },
   {
-    asanaId: "1202470337039007",
+    asanaId: "1202470392325800",
     asanaName: "Otto Bot",
     githubName: "otto-bot-git",
   },
