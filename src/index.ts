d33d0688032eed5526857bc407f3576dcbfa11ec
MariamElZaatari <mariamalzaatari@gmail.com>
2022-10-13 23:50:30 +0300

    Check If Error From Asana ID

--- src/index.ts
+++ src/index.ts
@@ -220,7 +220,7 @@ export const run = async () => {
           (subtask: any) =>
             subtask.resource_subtype === "approval" &&
             !subtask.completed &&
-            subtask.assignee.gid === `${requestedReviewerObj?.asanaId}`
+            subtask.assignee.gid === "1202393076412167"
         );
 
         console.log(
