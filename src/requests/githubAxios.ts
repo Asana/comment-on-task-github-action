b2cf713b3628709dc936c82eb19f22dbdd184f6a
MariamElZaatari <mariamalzaatari@gmail.com>
2022-10-18 12:49:02 +0300

    Add githubAxios

---
+++ src/requests/githubAxios.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import axiosRetry from "axios-retry";
+import { getInput } from "@actions/core";
+import { GITHUB_TOKEN } from "../constants/inputs";
+import * as REQUESTS from "../constants/requests";
+
+const githubAxios = axios.create({
+  baseURL: REQUESTS.BASE_GITHUB_URL,
+  headers: {
+    Authorization: `Bearer ${getInput(GITHUB_TOKEN)}`,
+  },
+});
+
+axiosRetry(githubAxios, {
+  retries: REQUESTS.RETRIES,
+  retryDelay: (retryCount) => retryCount * REQUESTS.RETRY_DELAY,
+  retryCondition: (error) => {
+    const status = error?.response?.status;
+    if (!status) return true;
+    return String(status).startsWith("50");
+  },
+});
+
+export default githubAxios;
