f277c436c5fdc4d403437bd2ca3d46d27b670ad3
MariamElZaatari <mariamalzaatari@gmail.com>
2022-11-16 20:37:53 +0200

    Rename Github token to Github PAT

--- src/constants/inputs.ts
+++ src/constants/inputs.ts
@@ -1,7 +1,7 @@
 export const COMMENT_TEXT = "comment-text";
 export const ASANA_SECRET = "asana-secret";
 export const ASANA_PAT = "asana-pat";
-export const GITHUB_TOKEN = "github-token";
+export const GITHUB_PAT = "github-pat";
 export const ALLOWED_PROJECTS = "allowed-projects";
 export const BLOCKED_PROJECTS = "blocked-projects";
 export const ACTION_URL = "action-url";
