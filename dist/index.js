491ac23ec11c78fbaa82702da4f086d111cd097d
MariamElZaatari <mariamalzaatari@gmail.com>
2023-09-01 01:46:32 +0300

    Clean Up

--- dist/index.js
+++ dist/index.js
@@ -15302,28 +15302,11 @@ const run = () => __awaiter(void 0, void 0, void 0, function* () {
                 const githubUrl = `${REPOS_URL}${repoName}${PULLS_URL}${pullRequestId}`;
                 let pullRequestDescription = (yield requests_githubAxios.get(githubUrl).then((response) => response.data.body)) || "";
                 let body = "";
-                if (pullRequestId === 1101) {
-                    console.log("OLD DESC");
-                    console.log(pullRequestDescription);
-                    console.log("NEW DESC");
-                    console.log(new_pr_description);
-                }
                 if (pullRequestDescription === null || pullRequestDescription === void 0 ? void 0 : pullRequestDescription.includes("A list of unique sandbox sites was created")) {
-                    if (pullRequestId === 1101) {
-                        console.log("BODY ALREADY HAD ZIPS");
-                    }
                     body = pullRequestDescription.replace(/## CI\/QA Testing Sandbox(.|\n|\r)*Please comment and open a new review on this pull request if you find any issues when testing the preview release zip files./ig, new_pr_description);
                 }
                 else {
-                    if (pullRequestId === 1101) {
-                        console.log("BODY DOES NOT HAVE ZIPS");
-                    }
                     body = (pullRequestDescription === null || pullRequestDescription === void 0 ? void 0 : pullRequestDescription.concat("\n\n" + new_pr_description)) || "";
-                }
-                if (pullRequestId === 1101) {
-                    console.log("RESULT");
-                    console.log(body);
-                    throw new Error("SOMETHING WENT WRONG");
                 }
                 yield requests_githubAxios.patch(githubUrl, {
                     body
